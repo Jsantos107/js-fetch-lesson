e7e0bcfabac36ff899a96e1fca75e960f3e9901d
Stephanie Jones <stephanie@Stephanies-MacBook-Pro.local>
2020-01-15 12:05:13 -0700

    completed code, ready for styling

--- app.js
+++ app.js
@@ -1,18 +1,21 @@
 const BASE_URL = 'https://dogs-backend.herokuapp.com/dogs'
+const dogsContainer = document.querySelector('.dogs-container')
+const dogForm = document.querySelector('#dog-form')
 
 fetch(BASE_URL)
     .then(parseJSON)
     .then(createDogs)
     .catch(console.log)
+
 function parseJSON(response) {
     return response.json()
 }
+
 function createDogs(dogs) {
     dogs.map(dogCards)
 }
-function dogCards(dog) {
 
-    const dogsContainer = document.querySelector('.dogs-container')
+function dogCards(dog) {
     const dogInfo = document.createElement('div')
     dogInfo.className = 'dog-info'
     dogInfo.innerHTML = `
@@ -21,34 +24,39 @@ function dogCards(dog) {
         `
     const deleteButton = document.createElement('button')
     deleteButton.innerText = 'DELETE'
-    deleteButton.addEventListener('click', (event) => {
+    deleteButton.addEventListener('click', () => {
         event.target.parentNode.remove()
         fetch(`${BASE_URL}/${dog.id}`, {
             method: 'DELETE'
         })
     })
+
     dogInfo.appendChild(deleteButton)
     dogsContainer.appendChild(dogInfo)
 }
 
-const dogForm = document.querySelector('#dog-form')
-dogForm.addEventListener('submit', event => {
+dogForm.addEventListener('submit', () => {
     event.preventDefault()
+
     const formData = new FormData(dogForm)
     const name = formData.get("name")
     const breed = formData.get("breed")
     const image = formData.get("image")
     const age = formData.get("age")
+
     dogCards({ name, breed, image, age })
+    postDog({ name, breed, image, age })
+})
+
+function postDog(dog) {
     fetch(BASE_URL, {
         method: "POST",
         headers: {
             'Accept': 'application/json',
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify({ name, breed, image, age }),
-    }).then(response => response.json())
+        body: JSON.stringify(dog),
+    }).then(parseJSON)
         .then(console.log)
-
-})
+}
 
