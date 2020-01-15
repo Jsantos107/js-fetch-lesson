718fbea126f1ea007c632833a38315261d0536ec
Stephanie Jones <stephanie@Stephanies-MacBook-Pro.local>
2020-01-15 12:22:55 -0700

    Solution code removed; instructions for project added

--- app.js
+++ app.js
@@ -1,61 +1,17 @@
-const BASE_URL = 'https://dogs-backend.herokuapp.com/dogs'
-const dogsContainer = document.querySelector('.dogs-container')
-const dogForm = document.querySelector('#dog-form')
+//fetch dogs
+//create dog cards for each returned dog object:
+{/* <div class="dog-info">
+    <img src="source here" />
+    <h1>Dog Name: name here</h1>
+    <button>DELETE</button>
+</div> */}
 
-fetch(BASE_URL)
-    .then(parseJSON)
-    .then(createDogs)
-    .catch(console.log)
+//add an event listener to the html form
+//on submit, dog should 
+    //1) be optimistically rendered to UI
+    //2) be posted to backend
+    //3) form should be cleared
 
-function parseJSON(response) {
-    return response.json()
-}
-
-function createDogs(dogs) {
-    dogs.map(dogCards)
-}
-
-function dogCards(dog) {
-    const dogInfo = document.createElement('div')
-    dogInfo.className = 'dog-info'
-    dogInfo.innerHTML = `
-        <img src=${dog.image} />
-        <h1>Dog Name: ${dog.name}</h1>
-        `
-    const deleteButton = document.createElement('button')
-    deleteButton.innerText = 'DELETE'
-    deleteButton.addEventListener('click', () => {
-        event.target.parentNode.remove()
-        fetch(`${BASE_URL}/${dog.id}`, {
-            method: 'DELETE'
-        })
-    })
-
-    dogInfo.appendChild(deleteButton)
-    dogsContainer.appendChild(dogInfo)
-}
-
-dogForm.addEventListener('submit', () => {
-    event.preventDefault()
-
-    const formData = new FormData(dogForm)
-    const name = formData.get("name")
-    const breed = formData.get("breed")
-    const image = formData.get("image")
-    const age = formData.get("age")
-
-    dogCards({ name, breed, image, age })
-    postDog({ name, breed, image, age })
-})
-
-function postDog(dog) {
-    fetch(BASE_URL, {
-        method: "POST",
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(dog),
-    }).then(dogForm.reset())
-}
-
+//add event listener to delete button
+//optimistically render deleted dog
+//delete from backend
